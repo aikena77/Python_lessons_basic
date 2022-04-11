3b9072583682a66ef190b61d579aecbd7c161eec
aikena77 <aikena77@mail.ru>
2022-04-11 11:16:23 +0600

    добавила print

--- lesson_2/Primer_1.py
+++ lesson_2/Primer_1.py
@@ -9,6 +9,7 @@ if age > 16 and country == "Kazaxstan":
         print("Goodbuy")
 else:
     print("SSORY")
+    print("Good Luck")
 print("Ваше имя %s, Ваш возвраст %d " % (name, age)) # %s форматирование строки   %d - форматирование чисел
 print("Ваше имя {}, Ваш возвраст {} " .format (name, age)) # современный способ
 print(f"Ваше имя {name}, Ваш возвраст {age}")  # еще лучше способ форматирования
