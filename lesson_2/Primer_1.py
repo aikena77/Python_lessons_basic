45f78223c74b0b67d61edb81d4327c47a729dffa
aikena77 <aikena77@mail.ru>
2022-04-11 11:34:13 +0600

    Print перенесли

--- lesson_2/Primer_1.py
+++ lesson_2/Primer_1.py
@@ -3,13 +3,12 @@ country = "Kazaxstan"
 if age > 16 and country == "Kazaxstan":
     print("HELLO")
     name = input("Как вас зовут? ")
-    if name == "admin":
-        print("Hi, "  + name )
-    else:
+    if name != "admin":
         print("Goodbuy")
+    else:
+        print("Hi, "  + name )
+        print("Ваше имя %s, Ваш возвраст %d " % (name, age))  # %s форматирование строки   %d - форматирование чисел
+        print("Ваше имя {}, Ваш возвраст {} ".format(name, age))  # современный способ
+        print(f"Ваше имя {name}, Ваш возвраст {age}")  # еще лучше способ форматирования
 else:
-    print("SSORY")
-    print("Good Luck")
-print("Ваше имя %s, Ваш возвраст %d " % (name, age)) # %s форматирование строки   %d - форматирование чисел
-print("Ваше имя {}, Ваш возвраст {} " .format (name, age)) # современный способ
-print(f"Ваше имя {name}, Ваш возвраст {age}")  # еще лучше способ форматирования
+    print("SSORY ")
